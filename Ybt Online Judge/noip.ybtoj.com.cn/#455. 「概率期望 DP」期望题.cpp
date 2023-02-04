da9054e0ca07f1007a9a5633b9ceab1d9730c243
whitepaperdog <whitepaperdog@qq.com>
2023-02-04 20:40:17 +0800

    update 2023/2/4

---
+++ Ybt Online Judge/noip.ybtoj.com.cn/#455. 「概率期望 DP」期望题.cpp
@@ -0,0 +1,426 @@
+/**
+ *    unicode: UTF-8
+ *    name:    #455. 「概率期望 DP」期望题
+ *    author:  whitepaperdog (蒟蒻wjr)
+ *    located: Changle District, Fuzhou City, Fujian Province, China
+ *    created: 2023.02.04 周六 19:27:56 (Asia/Shanghai)
+ **/
+#include <cstdio>
+typedef long long ll;
+typedef unsigned long long ull;
+constexpr auto lowbit = [](const auto &x)
+{ return x & (-x); };
+
+// #define FAST_IO
+
+#if !defined(WIN32) && !defined(_WIN32)
+#define getchar getchar_unlocked
+#define putchar putchar_unlocked
+#endif
+namespace IO
+{
+#ifdef FAST_IO
+#ifndef FAST_IN
+#define FAST_IN
+#endif
+#ifndef FAST_OUT
+#define FAST_OUT
+#endif
+#endif
+
+    namespace INPUT
+    {
+#ifdef FAST_IN
+#ifndef FAST_OUT_BUFFER_SIZE
+#define FAST_OUT_BUFFER_SIZE (1 << 21)
+#endif
+        char _buf[FAST_OUT_BUFFER_SIZE], *_now = _buf, *_end = _buf;
+#undef getchar
+#define getchar() (_now == _end && (_end = (_now = _buf) + fread(_buf, 1, FAST_OUT_BUFFER_SIZE, stdin), _now == _end) ? EOF : *_now++)
+#else
+#if !defined(WIN32) && !defined(_WIN32)
+#define getchar getchar_unlocked
+#endif
+#endif
+        inline void read(char &_x)
+        {
+            char _s = (char)getchar();
+            while ((_s >= 0x0a && _s <= 0x0d) || (_s == 0x09) || (_s == 0x20))
+                _s = (char)getchar();
+            _x = _s;
+        }
+        inline int read(char *_x)
+        {
+            char _s = (char)getchar();
+            int _len = 0;
+            while ((_s >= 0x0a && _s <= 0x0d) || (_s == 0x09) || (_s == 0x20))
+                _s = (char)getchar();
+            while (!((_s >= 0x0a && _s <= 0x0d) || (_s == 0x09) || (_s == 0x20)))
+            {
+                _x[_len++] = _s;
+                _s = (char)getchar();
+            }
+            _x[_len] = '\0';
+            return _len;
+        }
+        template <typename _T>
+        inline void read(_T &_x)
+        {
+            _x = 0;
+            char _s = (char)getchar();
+            bool _f = false;
+            while (!(_s >= '0' && _s <= '9'))
+            {
+                if (_s == '-')
+                    _f = true;
+                _s = (char)getchar();
+            }
+            while (_s >= '0' && _s <= '9')
+            {
+                _x = (_x << 1) + (_x << 3) + (_s - '0');
+                _s = (char)getchar();
+            }
+            if (_f)
+                _x = -_x;
+        }
+        template <typename _T, typename... _G>
+        inline void read(_T &_x, _G &..._y)
+        {
+            read(_x);
+            read(_y...);
+        }
+#ifdef FAST_IN
+#undef getchar
+#if !defined(WIN32) && !defined(_WIN32)
+#define getchar getchar_unlocked
+#endif
+#endif
+    }
+    namespace OUTPUT
+    {
+#ifdef FAST_OUT
+#ifndef FAST_OUT_BUFFER_SIZE
+#define FAST_OUT_BUFFER_SIZE (1 << 21)
+#endif
+        char _buf[FAST_OUT_BUFFER_SIZE], *_now = _buf;
+        inline void flush()
+        {
+            fwrite(_buf, 1, _now - _buf, stdout), _now = _buf;
+        }
+#undef putchar
+#define putchar(c) (_now - _buf == FAST_OUT_BUFFER_SIZE ? flush(), *_now++ = c : *_now++ = c)
+#else
+#if !defined(WIN32) && !defined(_WIN32)
+#define putchar putchar_unlocked
+#endif
+#endif
+        inline void write(char _x)
+        {
+            putchar(_x);
+        }
+        inline void write(char *const _x)
+        {
+            for (int i = 0; _x[i]; ++i)
+                putchar(_x[i]);
+        }
+        inline void write(const char *__restrict _x)
+        {
+            for (int i = 0; _x[i]; ++i)
+                putchar(_x[i]);
+        }
+        template <typename _T>
+        inline void write(_T _x)
+        {
+            static char dight[39];
+            int _len = 0;
+            if (_x == 0)
+            {
+                putchar('0');
+                return;
+            }
+            bool flag = false;
+            if (_x < 0)
+            {
+                _x = -_x;
+                flag = true;
+            }
+            while (_x)
+            {
+                dight[_len++] = (char)(_x % 10) + '0';
+                _x /= 10;
+            }
+            if (flag)
+                putchar('-');
+            while (_len--)
+                putchar(dight[_len]);
+        }
+        template <typename _T, typename... _G>
+        inline void write(_T _x, _G... _y)
+        {
+            write(_x);
+            write(_y...);
+        }
+#ifdef FAST_OUT
+#undef putchar
+#if !defined(WIN32) && !defined(_WIN32)
+#define putchar putchar_unlocked
+#endif
+#endif
+    }
+}
+template <typename T>
+inline void ckmin(T &_x, T _y)
+{
+    if (_x > _y)
+        _x = _y;
+}
+template <typename T>
+inline void ckmax(T &_x, T _y)
+{
+    if (_x < _y)
+        _x = _y;
+}
+template <const int _mod>
+struct modint
+{
+    int x;
+    constexpr modint(int _x = 0) : x(_x) {}
+    constexpr inline modint operator+() const
+    {
+        return *this;
+    }
+    constexpr inline modint operator-() const
+    {
+        return !x ? 0 : _mod - x;
+    }
+    constexpr inline modint &operator++()
+    {
+        ++x;
+        if (x >= _mod)
+            x -= _mod;
+        return *this;
+    }
+    constexpr inline modint &operator--()
+    {
+        --x;
+        if (x < 0)
+            x += _mod;
+        return *this;
+    }
+    constexpr inline modint operator++(int)
+    {
+        int res = x;
+        if (x >= _mod)
+            x -= _mod;
+        return res;
+    }
+    constexpr inline modint operator--(int)
+    {
+        int res = x;
+        if (x < 0)
+            x += _mod;
+        return res;
+    }
+    constexpr inline modint operator+(const modint &rhs) const
+    {
+        int res = x;
+        res += rhs.x;
+        if (res >= _mod)
+            res -= _mod;
+        return res;
+    }
+    constexpr inline modint operator-(const modint &rhs) const
+    {
+        int res = x;
+        res -= rhs.x;
+        if (res < 0)
+            res += _mod;
+        return res;
+    }
+    constexpr inline modint operator*(const modint &rhs) const
+    {
+        return (int)((ll)x * rhs.x % _mod);
+    }
+    constexpr inline modint &operator+=(const modint &rhs)
+    {
+        x += rhs.x;
+        if (x >= _mod)
+            x -= _mod;
+        return *this;
+    }
+    constexpr inline modint &operator-=(const modint &rhs)
+    {
+        x -= rhs.x;
+        if (x < 0)
+            x += _mod;
+        return *this;
+    }
+    constexpr inline modint &operator*=(const modint &rhs)
+    {
+        x = (int)((ll)x * rhs.x % _mod);
+        return *this;
+    }
+    template <typename _G>
+    constexpr inline modint operator^(_G rhs) const
+    {
+        modint a = x, res = 1;
+        while (rhs)
+        {
+            if (rhs & 1)
+                res *= a;
+            a *= a;
+            rhs >>= 1;
+        }
+        return res;
+    }
+    constexpr inline modint inv() const
+    {
+        return *this ^ (_mod - 2);
+    }
+    constexpr inline modint operator/(const modint &rhs) const
+    {
+        return (*this) * rhs.inv();
+    }
+    constexpr inline modint &operator/=(const modint &rhs)
+    {
+        return (*this) *= rhs.inv();
+    }
+    constexpr inline modint &operator==(const modint &rhs)
+    {
+        return x == rhs.x;
+    }
+    constexpr inline int &data()
+    {
+        return x;
+    }
+};
+template <typename _T, const int MAXN, const int MAXM>
+struct Graph
+{
+    struct Edge
+    {
+        int next;
+        _T to;
+    } edge[MAXM];
+    int head[MAXN], num_edge;
+    inline void add_edge(int from, _T to)
+    {
+        edge[++num_edge].next = head[from];
+        edge[num_edge].to = to;
+        head[from] = num_edge;
+    }
+#define foreach(i, graph, u) for (int i = graph.head[u]; i; i = graph.edge[i].next)
+};
+using IO::INPUT::read;
+using IO::OUTPUT::write;
+using namespace std;
+constexpr int N = 1e5 + 5;
+constexpr int mod = 998244353;
+typedef modint<mod> node;
+int n;
+char str[N];
+struct Edge
+{
+    int next, to;
+} edge[N * 2];
+int head[N], num_edge;
+inline void add_edge(int from, int to)
+{
+    edge[++num_edge].next = head[from];
+    edge[num_edge].to = to;
+    head[from] = num_edge;
+}
+int dep[N];
+node inv[N];
+node dp[N], f[N], g[N], h[N];
+inline node dfs(int u, int _fa)
+{
+    if (dep[u] == 1)
+    {
+        dp[u] = (str[u] == '1');
+        return 0;
+    }
+    node res = 0;
+    for (int i = head[u]; i; i = edge[i].next)
+    {
+        int v = edge[i].to;
+        if (v == _fa)
+            continue;
+        res += dfs(v, u);
+        dp[u] += dp[v];
+    }
+    node times = ((node)1 - res * inv[dep[u]]).inv();
+    dp[u] = (dp[u] * inv[dep[u]] + (str[u] == '1')) * times;
+    return inv[dep[u]] * times;
+}
+inline void dfs1(int u, int _fa)
+{
+    if (dep[u] == 1)
+        return;
+    node res = 0;
+    for (int i = head[u]; i; i = edge[i].next)
+    {
+        int v = edge[i].to;
+        if (v == _fa)
+            continue;
+        dfs1(v, u);
+        res += h[v];
+    }
+    h[u] = inv[dep[u]] / ((node)1 - res * inv[dep[u]]);
+}
+inline void dfs2(int u, int _fa)
+{
+    node res = g[u];
+    for (int i = head[u]; i; i = edge[i].next)
+    {
+        int v = edge[i].to;
+        if (v == _fa)
+            continue;
+        res += h[v];
+    }
+    for (int i = head[u]; i; i = edge[i].next)
+    {
+        int v = edge[i].to;
+        if (v == _fa)
+            continue;
+        g[v] = inv[dep[u]] / ((node)1 - (res - h[v]) * inv[dep[u]]);
+        f[v] = f[u] * g[v];
+        dfs2(v, u);
+    }
+}
+signed main()
+{
+#ifdef PAPERDOG
+    freopen("project.in", "r", stdin);
+    freopen("project.out", "w", stdout);
+#else
+    freopen("tree.in", "r", stdin);
+    freopen("tree.out", "w", stdout);
+#endif
+    read(n);
+    read(str + 1);
+    inv[1] = 1;
+    for (int i = 2; i <= n; ++i)
+        inv[i] = -(node)(mod / i) * inv[mod % i];
+    for (int i = 1; i < n; ++i)
+    {
+        int u, v;
+        read(u, v);
+        add_edge(u, v);
+        add_edge(v, u);
+        ++dep[u], ++dep[v];
+    }
+    dfs(1, 0);
+    f[1] = 1, g[1] = 0, h[1] = 0;
+    dfs1(1, 0);
+    dfs2(1, 0);
+    node res = dp[1];
+    for (int i = 1; i <= n; ++i)
+        if (str[i] == '0')
+            res += f[i];
+    write(res.data(), '\n');
+#ifdef FAST_OUT
+    IO::OUTPUT::flush();
+#endif
+    return 0;
+}
---
+++ Ybt Online Judge/noip.ybtoj.com.cn/#681. 「线段树合并」图论题.cpp
@@ -0,0 +1,435 @@
+/**
+ *    unicode: UTF-8
+ *    name:    #681. 「线段树合并」图论题
+ *    author:  whitepaperdog (蒟蒻wjr)
+ *    located: Changle District, Fuzhou City, Fujian Province, China
+ *    created: 2023.02.04 周六 17:52:33 (Asia/Shanghai)
+ **/
+#include <algorithm>
+#include <cstdio>
+typedef long long ll;
+typedef unsigned long long ull;
+constexpr auto lowbit = [](const auto &x) { return x & (-x); };
+
+#define FAST_IO
+
+#if !defined(WIN32) && !defined(_WIN32)
+#define getchar getchar_unlocked
+
+#define putchar putchar_unlocked
+
+#endif
+namespace IO
+{
+#ifdef FAST_IO
+#ifndef FAST_IN
+#define FAST_IN
+
+#endif
+#ifndef FAST_OUT
+#define FAST_OUT
+
+#endif
+#endif
+
+    namespace INPUT
+    {
+#ifdef FAST_IN
+#ifndef FAST_OUT_BUFFER_SIZE
+#define FAST_OUT_BUFFER_SIZE (1 << 21)
+
+#endif
+        char _buf[FAST_OUT_BUFFER_SIZE], *_now = _buf, *_end = _buf;
+#undef getchar
+#define getchar() (_now == _end && (_end = (_now = _buf) + fread(_buf, 1, FAST_OUT_BUFFER_SIZE, stdin), _now == _end) ? EOF : *_now++)
+
+#else
+#if !defined(WIN32) && !defined(_WIN32)
+#define getchar getchar_unlocked
+
+#endif
+#endif
+        inline void read(char &_x)
+        {
+            char _s = (char)getchar();
+            while ((_s >= 0x0a && _s <= 0x0d) || (_s == 0x09) || (_s == 0x20))
+                _s = (char)getchar();
+            _x = _s;
+        }
+        inline int read(char *_x)
+        {
+            char _s = (char)getchar();
+            int _len = 0;
+            while ((_s >= 0x0a && _s <= 0x0d) || (_s == 0x09) || (_s == 0x20))
+                _s = (char)getchar();
+            while (!((_s >= 0x0a && _s <= 0x0d) || (_s == 0x09) || (_s == 0x20)))
+            {
+                _x[_len++] = _s;
+                _s = (char)getchar();
+            }
+            _x[_len] = '\0';
+            return _len;
+        }
+        template <typename _T>
+        inline void read(_T &_x)
+        {
+            _x = 0;
+            char _s = (char)getchar();
+            bool _f = false;
+            while (!(_s >= '0' && _s <= '9'))
+            {
+                if (_s == '-')
+                    _f = true;
+                _s = (char)getchar();
+            }
+            while (_s >= '0' && _s <= '9')
+            {
+                _x = (_x << 1) + (_x << 3) + (_s - '0');
+                _s = (char)getchar();
+            }
+            if (_f)
+                _x = -_x;
+        }
+        template <typename _T, typename... _G>
+        inline void read(_T &_x, _G &..._y)
+        {
+            read(_x);
+            read(_y...);
+        }
+#ifdef FAST_IN
+#undef getchar
+#if !defined(WIN32) && !defined(_WIN32)
+#define getchar getchar_unlocked
+
+#endif
+#endif
+    }
+    namespace OUTPUT
+    {
+#ifdef FAST_OUT
+#ifndef FAST_OUT_BUFFER_SIZE
+#define FAST_OUT_BUFFER_SIZE (1 << 21)
+
+#endif
+        char _buf[FAST_OUT_BUFFER_SIZE], *_now = _buf;
+        inline void flush()
+        {
+            fwrite(_buf, 1, _now - _buf, stdout), _now = _buf;
+        }
+#undef putchar
+#define putchar(c) (_now - _buf == FAST_OUT_BUFFER_SIZE ? flush(), *_now++ = c : *_now++ = c)
+
+#else
+#if !defined(WIN32) && !defined(_WIN32)
+#define putchar putchar_unlocked
+
+#endif
+#endif
+        inline void write(char _x)
+        {
+            putchar(_x);
+        }
+        inline void write(char *const _x)
+        {
+            for (int i = 0; _x[i]; ++i)
+                putchar(_x[i]);
+        }
+        inline void write(const char *__restrict _x)
+        {
+            for (int i = 0; _x[i]; ++i)
+                putchar(_x[i]);
+        }
+        template <typename _T>
+        inline void write(_T _x)
+        {
+            static char dight[39];
+            int _len = 0;
+            if (_x == 0)
+            {
+                putchar('0');
+                return;
+            }
+            bool flag = false;
+            if (_x < 0)
+            {
+                _x = -_x;
+                flag = true;
+            }
+            while (_x)
+            {
+                dight[_len++] = (char)(_x % 10) + '0';
+                _x /= 10;
+            }
+            if (flag)
+                putchar('-');
+            while (_len--)
+                putchar(dight[_len]);
+        }
+        template <typename _T, typename... _G>
+        inline void write(_T _x, _G... _y)
+        {
+            write(_x);
+            write(_y...);
+        }
+#ifdef FAST_OUT
+#undef putchar
+#if !defined(WIN32) && !defined(_WIN32)
+#define putchar putchar_unlocked
+
+#endif
+#endif
+    }
+}
+template <typename T>
+inline void ckmin(T &_x, T _y)
+{
+    if (_x > _y)
+        _x = _y;
+}
+template <typename T>
+inline void ckmax(T &_x, T _y)
+{
+    if (_x < _y)
+        _x = _y;
+}
+template <const int _mod>
+struct modint
+{
+    int x;
+    constexpr modint(int _x = 0) : x(_x) {}
+    constexpr inline modint operator+() const
+    {
+        return *this;
+    }
+    constexpr inline modint operator-() const
+    {
+        return !x ? 0 : _mod - x;
+    }
+    constexpr inline modint &operator++()
+    {
+        ++x;
+        if (x >= _mod)
+            x -= _mod;
+        return *this;
+    }
+    constexpr inline modint &operator--()
+    {
+        --x;
+        if (x < 0)
+            x += _mod;
+        return *this;
+    }
+    constexpr inline modint operator++(int)
+    {
+        int res = x;
+        if (x >= _mod)
+            x -= _mod;
+        return res;
+    }
+    constexpr inline modint operator--(int)
+    {
+        int res = x;
+        if (x < 0)
+            x += _mod;
+        return res;
+    }
+    constexpr inline modint operator+(const modint &rhs) const
+    {
+        int res = x;
+        res += rhs.x;
+        if (res >= _mod)
+            res -= _mod;
+        return res;
+    }
+    constexpr inline modint operator-(const modint &rhs) const
+    {
+        int res = x;
+        res -= rhs.x;
+        if (res < 0)
+            res += _mod;
+        return res;
+    }
+    constexpr inline modint operator*(const modint &rhs) const
+    {
+        return (int)((ll)x * rhs.x % _mod);
+    }
+    constexpr inline modint &operator+=(const modint &rhs)
+    {
+        x += rhs.x;
+        if (x >= _mod)
+            x -= _mod;
+        return *this;
+    }
+    constexpr inline modint &operator-=(const modint &rhs)
+    {
+        x -= rhs.x;
+        if (x < 0)
+            x += _mod;
+        return *this;
+    }
+    constexpr inline modint &operator*=(const modint &rhs)
+    {
+        x = (int)((ll)x * rhs.x % _mod);
+        return *this;
+    }
+    template <typename _G>
+    constexpr inline modint operator^(_G rhs) const
+    {
+        modint a = x, res = 1;
+        while (rhs)
+        {
+            if (rhs & 1)
+                res *= a;
+            a *= a;
+            rhs >>= 1;
+        }
+        return res;
+    }
+    constexpr inline modint inv() const
+    {
+        return *this ^ (_mod - 2);
+    }
+    constexpr inline modint operator/(const modint &rhs) const
+    {
+        return (*this) * rhs.inv();
+    }
+    constexpr inline modint &operator/=(const modint &rhs)
+    {
+        return (*this) *= rhs.inv();
+    }
+    constexpr inline modint &operator==(const modint &rhs)
+    {
+        return x == rhs.x;
+    }
+    constexpr inline int &data()
+    {
+        return x;
+    }
+};
+template <typename _T, const int MAXN, const int MAXM>
+struct Graph
+{
+    struct Edge
+    {
+        int next;
+        _T to;
+    } edge[MAXM];
+    int head[MAXN], num_edge;
+    inline void add_edge(int from, _T to)
+    {
+        edge[++num_edge].next = head[from];
+        edge[num_edge].to = to;
+        head[from] = num_edge;
+    }
+#define foreach(i, graph, u) for (int i = graph.head[u]; i; i = graph.edge[i].next)
+
+};
+using IO::INPUT::read;
+using IO::OUTPUT::write;
+using namespace std;
+constexpr int N = 1e6 + 5;
+constexpr int mod = 998244353;
+int n, m;
+struct Edge
+{
+    int next, to;
+} edge[N];
+int head[N], num_edge;
+inline void add_edge(int from, int to)
+{
+    edge[++num_edge].next = head[from];
+    edge[num_edge].to = to;
+    head[from] = num_edge;
+}
+struct Tree
+{
+    int l, r;
+    int size;
+} tree[N * 60];
+#define lc(rt) tree[rt].l
+
+#define rc(rt) tree[rt].r
+
+int root[N], cnt;
+inline void pushup(int rt)
+{
+    tree[rt].size = tree[lc(rt)].size + tree[rc(rt)].size;
+}
+inline void update(int &rt, int l, int r, int pos)
+{
+    if (!rt)
+        rt = ++cnt;
+    ++tree[rt].size;
+    if (l == r)
+        return;
+    int mid = (l + r) >> 1;
+    if (pos <= mid)
+        update(lc(rt), l, mid, pos);
+    else
+        update(rc(rt), mid + 1, r, pos);
+}
+inline int merge(int x, int y, int l, int r)
+{
+    if (!x || !y)
+        return x | y;
+    if (l == r)
+        return x;
+    int mid = (l + r) >> 1;
+    lc(x) = merge(lc(x), lc(y), l, mid);
+    rc(x) = merge(rc(x), rc(y), mid + 1, r);
+    pushup(x);
+    return x;
+}
+inline int query(int rt, int l, int r, int x, int y)
+{
+    if (r < x || l > y)
+        return 0;
+    if (x <= l && r <= y)
+        return tree[rt].size;
+    int mid = (l + r) >> 1;
+    return query(lc(rt), l, mid, x, y) + query(rc(rt), mid + 1, r, x, y);
+}
+int fa[N];
+inline int find(int x)
+{
+    return !fa[x] ? x : fa[x] = find(fa[x]);
+}
+signed main()
+{
+#ifdef PAPERDOG
+    freopen("project.in", "r", stdin);
+    freopen("project.out", "w", stdout);
+#else
+    freopen("graph.in", "r", stdin);
+    freopen("graph.out", "w", stdout);
+#endif
+    read(n, m);
+    for (int i = 1; i <= m; ++i)
+    {
+        int u, v;
+        read(u, v);
+        if (u > v)
+            swap(u, v);
+        add_edge(v, u);
+        update(root[u], 1, n, v);
+    }
+    modint<mod> ans = n;
+    for (int u = 1; u < n; ++u)
+    {
+        int fau = find(u);
+        for (int i = head[u]; i; i = edge[i].next)
+        {
+            int v = edge[i].to;
+            int fav = find(v);
+            if (fau == fav)
+                continue;
+            root[fa[fav] = fau] = merge(root[fau], root[fav], 1, n);
+        }
+        ans *= (n - query(root[fau], 1, n, u + 1, n));
+    }
+    write(ans.data(), '\n');
+#ifdef FAST_OUT
+    IO::OUTPUT::flush();
+#endif
+    return 0;
+}
